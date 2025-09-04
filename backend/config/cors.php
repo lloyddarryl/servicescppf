bf3462acd8d06849d1019d1fe138fc8fbcff75ff
lloyddarryl <lloyddarrylobg@gmail.com>
2025-09-04 10:14:10 +0100

    okkk

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,8 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:3000', 'http://localhost:8000','http://127.0.0.1:3000','http://localhost:3001',
- 'https://*.cppf-services.com'],
+ 'https://*.cppf-services.com','https://servicescppf.vercel.app',
+],
 
     'allowed_origins_patterns' => ['/^https:\/\/.+\.cppf-services\.com$/'],
 
